af341fb294579ad441e3c1cc5e5093db6547dd6c
Noella-Ntare <n.ntare@alustudent.com>
2026-02-10 12:29:36 +0200

    question2

--- Question2/question2_numbers.c
+++ Question2/question2_numbers.c
@@ -1,67 +1,51 @@
-// C code
-// Simple Menu Program
-
 #include <stdio.h>
 
 int main() {
-    int choice, num, i, sum;
-    char again;
+    int choice, n, i, sum;
     
-    do {
-        // the menu
+    while (1) {  // Repetition
+        
+        // prompt user to choose
         printf("\n--- MENU ---\n");
         printf("1. Sum of numbers\n");
         printf("2. Even numbers\n");
-        printf("3. Multiplication table\n");
-        printf("4. Exit\n");
-        printf("Enter choice: ");
+        printf("3. Exit\n");
+        printf("Choice: ");
         scanf("%d", &choice);
         
+        
         if (choice == 1) {
-            // Sum 
-            printf("Enter a number: ");
-            scanf("%d", &num);
+            // Sum from 1 to N
+            printf("Enter Number: ");
+            scanf("%d", &n);
             sum = 0;
-            for (i = 1; i <= num; i++) {
+            for (i = 1; i <= n; i++) {
                 sum = sum + i;
             }
             printf("Sum = %d\n", sum);
         }
         else if (choice == 2) {
-            //even numbers
-            printf("Enter a number: ");
-            scanf("%d", &num);
-            printf("Even numbers: ");
-            for (i = 1; i <= num; i++) {
+            // Print even numbers
+            printf("Enter N: ");
+            scanf("%d", &n);
+            printf("Even: ");
+            for (i = 1; i <= n; i++) {
                 if (i % 2 != 0) {
-                    continue;  // Skip the odd numbers
+                    continue; 
                 }
                 printf("%d ", i);
             }
             printf("\n");
         }
         else if (choice == 3) {
-            // Multiplication table
-            printf("Enter a number: ");
-            scanf("%d", &num);
-            for (i = 1; i <= 10; i++) {
-                printf("%d x %d = %d\n", num, i, num * i);
-            }
-        }
-        else if (choice == 4) {
+            // Exit program
             printf("Goodbye!\n");
-            break;  // Exit the loop
+            break; 
         }
         else {
-            printf("Invalid choice!\n");
+            printf("Invalid!\n");
         }
-        
-        if (choice != 4) {
-            printf("\nContinue? (y/n): ");
-            scanf(" %c", &again);
-        }
-        
-    } while (again == 'y' || again == 'Y');
+    }
     
     return 0;
 }
